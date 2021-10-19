1d49ec4c57d2c5806ef06fc3d64ce3911e5f27de
John Ratana <jratana1@gmail.com>
2021-10-19 17:56:23 -0400

    hospital provider rltn updates dynamically

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -13,10 +13,13 @@ class ApplicationRecord < ActiveRecord::Base
   end
 
   def self.destroy(id)
+    string = self.name.split /(?=[A-Z])/
+    table =string.join("_")
+    byebug
     query = "
     DELETE 
     FROM 
-      #{self.name + "s"}
+      #{table + "s"}
     WHERE
       Id = #{id}"
 
@@ -38,7 +41,7 @@ class ApplicationRecord < ActiveRecord::Base
     query = "
     UPDATE #{self.name + "s"}
     SET #{columns}
-    WHERE Id = #{record[:id]}"
+    WHERE Id = #{record["id"]}"
 
     ActiveRecord::Base.connection.execute(query)
   end
@@ -68,12 +71,21 @@ class ApplicationRecord < ActiveRecord::Base
   end
 
   def self.associate(table1, table2, id1, id2)
-    query = "
-    INSERT
-    INTO #{table1 + "_" + table2 + "s"}
-    (#{table1 + "_id"}, #{table2 + "_id"},created_at,updated_at)
-    VALUES
-    ('#{id1}','#{id2}','#{Time.current.utc}','#{Time.current.utc}');"
+
+    query= "INSERT INTO
+    #{table1}_#{table2}s (
+      #{table1}_id,
+      #{table2}_id,
+      created_at,
+      updated_at 
+    )
+    SELECT
+    '#{id1}',
+    '#{id2}',
+    '#{Time.current.utc}',
+    '#{Time.current.utc}'
+    WHERE
+    NOT EXISTS (SELECT * FROM #{table1}_#{table2}s WHERE #{table2}_id = '#{id2}' AND #{table1}_id = '#{id1}')"
 
     ActiveRecord::Base.connection.execute(query)
   end
@@ -86,4 +98,25 @@ class ApplicationRecord < ActiveRecord::Base
     
     ActiveRecord::Base.connection.execute(query)
   end
+
+  def self.get_all_associations(association, id)
+    table = ""
+    if self.name.downcase < association
+      table =  self.name + "_" + association + "s"
+    else
+      table =  association + "_" + self.name + "s"
+    end
+
+    query= "
+    SELECT
+    *
+    FROM
+      #{table}
+    WHERE #{self.name}_id = #{id}
+    ORDER BY created_at DESC"
+
+    ActiveRecord::Base.connection.execute(query)
+  end
+
+
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,41 @@
 class Provider < ApplicationRecord
+    def self.get_with_hospitals
+        providers_hash= {providers: []}
+
+        query = "
+        SELECT Providers.created_at, Providers.id, Providers.first_name, Providers.last_name, Hospitals.id AS Hospital_id, Hospitals.name
+        FROM Providers
+        LEFT JOIN Hospital_providers
+        ON Providers.Id=Hospital_providers.provider_id
+        LEFT JOIN Hospitals
+        ON Hospital_providers.hospital_id=Hospitals.Id
+        ORDER BY created_at DESC"
+
+        result = ActiveRecord::Base.connection.execute(query)
+
+        result.each do |row|
+            if providers_hash[:providers].any? {|h| h[:id] == row["id"]}
+                hospital = {
+                        id: row["hospital_id"],
+                        name: row["name"]
+                        }
+
+                match_index = providers_hash[:providers].index(providers_hash[:providers].find { |h| h[:id] == row["id"] })  
+                
+                providers_hash[:providers][match_index][:hospitals].push(hospital)
+            else
+                provider = {id: row["id"],
+                            last_name: row["last_name"],
+                            first_name: row["first_name"],
+                            hospitals: [{
+                                        id: row["hospital_id"],
+                                        name: row["name"]
+                                        }
+                                        ]
+                            }
+                providers_hash[:providers].push(provider)
+            end
+        end
+        providers_hash
+    end
 end
